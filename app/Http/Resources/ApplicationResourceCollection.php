091eba0ccde0dee3482449043d301f0cdb358069
vp33722 <vp33722@gmail.com>
2018-11-27 13:15:15 +0530

    save

--- app/Http/Resources/ApplicationResourceCollection.php
+++ app/Http/Resources/ApplicationResourceCollection.php
@@ -4,8 +4,8 @@ namespace App\Http\Resources;
 
 use App\Application;
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use Illuminate\Http\Resources\Json\JsonResource;
-
+//use Illuminate\Http\Resources\Json\JsonResource;
+use Symfony\Component\HttpFoundation\JsonResponse;
 class ApplicationResourceCollection extends JsonResource
 {
     /**
