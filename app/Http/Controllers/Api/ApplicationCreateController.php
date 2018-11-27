48dd3b9ecd6127e8da140200dd5c14f6f0a07a64
vp33722 <vp33722@gmail.com>
2018-11-27 12:45:41 +0530

    save

--- app/Http/Controllers/Api/ApplicationCreateController.php
+++ app/Http/Controllers/Api/ApplicationCreateController.php
@@ -15,7 +15,7 @@ class ApplicationCreateController extends Controller
 
         if (!empty($apps)) {
             
-            return new ApplicationResourceCollection(Application::where('app_platform_id', $request->get('app_platform_id'))->first());
+            return ApplicationResourceCollection::collection(Application::where('app_platform_id', $request->get('app_platform_id'))->first());
 
         }
         return response()->json([
