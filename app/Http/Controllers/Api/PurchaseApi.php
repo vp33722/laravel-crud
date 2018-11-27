6cbd0861139cd708caac0c06e868410aeb201551
vp33722 <vp33722@gmail.com>
2018-11-27 16:54:45 +0530

    save

---
+++ app/Http/Controllers/Api/PurchaseApi.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Http\Controllers\Api;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AppUserCollection;
+use Symfony\Component\HttpFoundation\JsonResponse;
+//use App\Http\Requests\Admin\Request\UserRequest;	
+use App\Appuser;		
+
+class PurchaseApi extends Controller
+{
+    public function updateApp(Request $request)
+    {
+        $users=[
+            "isPurchaseAds"=>"purchase_ads",
+            "isPurchaseWatermark"=>"is_purchase_watermark",
+            "isPurchaseUnlimited"=>"is_purchase_unlimited",
+            "isPurchaseSubscription"=>"is_purchase_subscription"
+              ];
+
+        if(array_key_exists($request->get('nameOfFlag'), $users))
+        { 
+         $users=Appuser::where('device_id',$request->get('device_id')->update(
+
+                [
+                    $users[$request->get('nameOfFlag')] => true,
+                ]
+
+                )); 
+        }
+
+        return $users;
+        /*return new JsonResponse([
+            'success' => false,
+            'Message' =>'Sorry Try Again',
+        ]);*/
+
+    }
+}
