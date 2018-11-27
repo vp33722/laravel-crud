44d7f346eebaf9bad83518270ea8ac79ccbb3b25
vp33722 <vp33722@gmail.com>
2018-11-27 12:26:04 +0530

    save

--- app/Http/Resources/ApplicationResourceCollection.php
+++ app/Http/Resources/ApplicationResourceCollection.php
@@ -22,21 +22,10 @@ class ApplicationResourceCollection extends ResourceCollection
 
        return
        [
-            'id'                            => $this->id,
-            'name'                          => $this->name,
-            'latestVersion'                 => $this->latest_version
-            /*'titleOfAd'                     => ($query->title_of_ad) ? $query->title_of_ad : '',
-            'messOfAd'                      => ($query->messge_of_ad) ? $query->messge_of_ad : '',
-            'link'                          => ($query->link) ? $query->link : '',
-            'contactEmail'                  => ($query->contact_email) ? $query->contact_email : '',
-            'shareFormat'                   => $query->share_format) ? $query->share_format : '',
-            'contactFormat'                 => ($query->contact_format) ? $query->contact_format :'',
-            'developerSite'                 => ($query->developer_site) ? $query->developer_site : '',
-            'developerName'                 => ($query->developer_name) ? $query->developer_name : ''
-            'developerApps'                 => ($query->developer_apps) ? $query->developer_apps : '',
-            'generatedInApp'                => ($query->generated_in_app) ? $query->generated_in_app : '',
-            'is_force_update'               => ($query->is_force_update) ? 'YES' : 'No',
-            'is_only_banner'                =>($query->is_only_banner) ? 'YES' : 'NO',*/
+            'id'                            => (string)$this->id,
+            'name'                          => (string)$this->name,
+            'latestVersion'                 => (string)$this->latest_version,
+            
        ];
 
     }
